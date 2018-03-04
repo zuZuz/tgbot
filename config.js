be0b4f3bb7fdd7e624c56242cdc7712c6147ebb8
Aydar <zreacta@gmail.com>
2018-03-04 21:14:24 +0300

    Load config from .env

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
+require('dotenv');
 const config = {};
 
-/*
-config.botToken = '500824875:AAH8FAKzrxhDgpDL6sOa3CFD3bZYPuqnG74';
-config.botDomain = 'livefood_bot';
-*/
-
-config.botToken = '470912859:AAE_kgtGhN4CTytMj2ScYYfZ8n9YG3Dh4xE';
-config.botDomain = 'gladoss_bot';
+config.botToken = process.env.BOT_TOKEN || '470912859:AAE_kgtGhN4CTytMj2ScYYfZ8n9YG3Dh4xE';
+config.botDomain = process.env.BOT_DOMAIN || 'gladoss_bot';
 config.inviteLink = 'https://t.me/joinchat/Ff6qslGKXErGsxAPDzpsqw';
 
 config.logDateFormat = 'DD-MM-YYYY hh:mm:ss';
@@ -16,10 +12,10 @@ config.logLevel = 'info';
 config.invoiceLink = 'https://livefood.store/?tg_id={0}';
 config.invoiceRK = 'https://livefood.store/?ps=rk&tg_id={0}';
 
-config.dbHost = '176.57.208.72';
-config.dbUser = 'responder';
-config.dbPass = 'YOaXicDdwxa4J68n';
-config.dbName = 'responder';
+config.dbHost = process.env.DB_HOST || '176.57.208.72';
+config.dbUser = process.env.DB_USER || 'responder';
+config.dbPass = process.env.DB_PASS || 'YOaXicDdwxa4J68n';
+config.dbName = process.env.DB_NAME || 'responder';
 
 config.httpToken = 'Gh75fe1r23';
 config.httpPort = 8080;
